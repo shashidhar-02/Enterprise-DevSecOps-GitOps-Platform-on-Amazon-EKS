b91e6ec94a414af1796426d56e641358f2327d11
shashidhar <s94094838@gmail.com>
2026-06-14 05:07:33 +0000

    Remove all project files

--- apps/backend/src/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export async function connectDatabase(uri) {
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(uri, {
-    serverSelectionTimeoutMS: 5000,
-  });
-}
--- scripts/setup-git-hooks.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-git config core.hooksPath .githooks
-echo "Git hooks path configured to .githooks"
